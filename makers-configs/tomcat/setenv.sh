5215ebf3ba0b8bd5b784b63199bafc8d3eeafa22
Kai Nguyen <kien.nt@live.com>
2021-04-23 12:26:56 +0930

    Added makers-configs

---
+++ makers-configs/tomcat/setenv.sh
@@ -0,0 +1,9 @@
+# How to use:
+#   Copy this file to your path/to/tomcat/libexec/bin folder
+#   chmod +x ./setenv.sh
+#   You might want to change the location of you JavaHome
+
+# Set default JavaHome to Java8
+export JAVA_HOME=/usr/local/opt/openjdk@8/libexec/openjdk.jdk/Contents/Home
+
+# For other settigns, check out https://gist.github.com/terrancesnyder/986029/5e13ca141dc8fd6d52c4169fb052bc898877c58c
